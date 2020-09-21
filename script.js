66fd9c47f2243825cc95453ed0a8356283426a51
Peter <99065921@mydavinci.nl>
2020-09-21 12:06:32 +0200

    Made input Case insensitive

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var geel = prompt("Is de kaas geel? (ja/nee)");
+geel = geel.toLowerCase();
 	if(geel == "ja") 
 	{
 		var gaten = prompt("Zitten er gaten in? (ja/nee)");
+    gaten = gaten.toLowerCase();
 		if(gaten == "ja")
 		{
 			var duur = prompt("Is de kaas belachelijk duur? (ja/nee)")
+      duur = duur.toLowerCase();
 			if(duur == "ja")
 			{
 				alert("De kaas die u zoekt is Emmenthaler.");
@@ -21,6 +24,7 @@ var geel = prompt("Is de kaas geel? (ja/nee)");
 		else if(gaten == "nee")
 		{
 			var hard = prompt("Is de kaas hard als steen? (ja/nee)");
+      hard = hard.toLowerCase();
 			if(hard == "ja")
 			{
 				alert("De kaas die u zoekt is Panmigiano Reggiano.");
@@ -42,9 +46,11 @@ var geel = prompt("Is de kaas geel? (ja/nee)");
 	else if(geel == "nee") 
 	{
 		var schimmel = prompt("Heeft de kaas blauwe schimmels? (ja/nee)");
+    schimmel = schimmel.toLowerCase();
 		if(schimmel == "ja")
 		{
 			var korst = prompt("Heeft de kaas een korst? (ja/nee)");
+      korst = korst.toLowerCase();
 			if(korst == "ja")
 			{
 				alert("De kaas die u zoekt is Bleu de Rochbaron.");
@@ -61,6 +67,7 @@ var geel = prompt("Is de kaas geel? (ja/nee)");
 		else if(schimmel == "nee")
 		{
 			var korst2 = prompt("Heeft de kaas een korst? (ja/nee)");
+      korst2 = korst2.toLowerCase();
 			if(korst2 == "ja")
 			{
 				alert("De kaas die u zoekt is Camembert");
